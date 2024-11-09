a24a8e2e3fe84057a04277f298b60956dea3b2c6
Jeremy Dufour <jeremy.k.dufour@gmail.com>
2024-11-09 10:34:14 -0500

    added implementation

--- src/main/kotlin/seg3x02/employeeGql/resolvers/EmployeesResolver.kt
+++ src/main/kotlin/seg3x02/employeeGql/resolvers/EmployeesResolver.kt
@@ -1,8 +1,35 @@
 package seg3x02.employeeGql.resolvers
 
+import org.springframework.data.mongodb.core.MongoOperations
+import org.springframework.graphql.data.method.annotation.Argument
+import org.springframework.graphql.data.method.annotation.MutationMapping
+import org.springframework.graphql.data.method.annotation.QueryMapping
+import org.springframework.graphql.data.method.annotation.SchemaMapping
 import org.springframework.stereotype.Controller
+import seg3x02.employeeGql.entity.Employee
+import seg3x02.employeeGql.repository.EmployeesRepository
+import seg3x02.employeeGql.resolvers.types.CreateEmployeeInput
+import java.util.UUID
 
 @Controller
-class EmployeesResolver {
+class EmployeesResolver(
+    private val employeesRepository: EmployeesRepository,
+    private val mongoOperations: MongoOperations
+) {
+    @QueryMapping
+    fun employees(): List<Employee> {
+        return employeesRepository.findAll()
+    }
+
+    @MutationMapping
+    fun addEmployee(@Argument("createEmployeeInput") input: CreateEmployeeInput): Employee {
+        if(input.name != null && input.dateOfBirth !=null && input.city != null && input.salary != null){
+            val employee: Employee = Employee(input.name, input.dateOfBirth, input.city, input.salary, input.gender, input.email)
+            employee.id = UUID.randomUUID().toString()
+            return employee
+        } else {
+            throw Exception("Invalid input")
+        }
+    }
 
 }
